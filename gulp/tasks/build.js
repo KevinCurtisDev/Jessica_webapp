d9a2d68ef977598b814ad8ae8ac0304cd1c5fe9a
kevin <kpcurtis@gmail.com>
2018-08-29 04:04:53 +0100

    Completed gulp build task

---
+++ gulp/tasks/build.js
@@ -0,0 +1,58 @@
+var gulp = require('gulp'),
+imagemin = require('gulp-imagemin'),
+del = require('del'),
+usemin = require('gulp-usemin'),
+rev = require('gulp-rev'),
+cssnano = require('gulp-cssnano'),
+uglify = require('gulp-uglify'),
+browserSync = require('browser-sync').create();
+
+gulp.task('previewDocs', function() {
+	browserSync.init({
+		notify: false,
+		server: {
+			baseDir: "docs"
+		}
+	});
+});
+
+gulp.task('deleteDocsFolder', function() {
+	return del("./docs");
+});
+
+gulp.task('copyGeneralFiles', ['deleteDocsFolder'], function() {
+	var pathsToCopy = [
+		'./app/**/*',
+		'!./app/**/*.html',
+		'!./app/Jess-Wireframe.pdf',
+		'!./app/assets/images/**',
+		'!./app/assets/styles/**',
+		'!./app/assets/js/**',
+		'!./app/temp',
+		'!./app/temp/**'
+	]
+
+	return gulp.src(pathsToCopy)
+		.pipe(gulp.dest("./docs"));
+});
+
+gulp.task('optimiseImages', ['deleteDocsFolder'], function() {
+	return gulp.src(['./app/assets/images/**/*'])
+		.pipe(imagemin({
+			progressive: true,
+			interlaced: true,
+			multipass: true
+		}))
+		.pipe(gulp.dest("./docs/assets/images"));
+});
+
+gulp.task('usemin', ['deleteDocsFolder', 'styles'], function() {
+	return gulp.src("./app/**/*.html")
+		.pipe(usemin({
+			css: [function() {return rev()}, function() {return cssnano()}],
+			js: [function() {return rev()}]
+		}))
+		.pipe(gulp.dest("./docs"));
+});
+
+gulp.task('build', ['deleteDocsFolder', 'copyGeneralFiles', 'optimiseImages', 'usemin']);
--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 require('./gulp/tasks/styles');
 require('./gulp/tasks/watch');
-
+require('./gulp/tasks/build');
 
 
